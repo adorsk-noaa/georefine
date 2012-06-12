4bcf2d7eb2d82d136a8719eec4aaf216bf72719f
adorsk <adorsk>
2012-06-12 16:45:11 -0400

    added sql fetching from dao

--- georefine/util/mapping/gt_renderer.py
+++ georefine/util/mapping/gt_renderer.py
@@ -4,11 +4,8 @@ import java.lang
 
 from java.io import File as JFile
 from java.util import HashMap
-from org.geotools.data import FileDataStoreFinder
 from org.geotools.data import DataStoreFinder
-from org.geotools.data.simple import SimpleFeatureSource
 from org.geotools.map import DefaultMapContext
-from org.geotools.swing import JMapFrame
 from org.geotools.renderer.lite import StreamingRenderer
 from java.awt.image import BufferedImage
 from java.awt import Rectangle
@@ -16,49 +13,27 @@ from java.awt import Color
 from java.io import ByteArrayOutputStream
 from javax.imageio import ImageIO
 from org.python.core.util import StringUtil
-from StringIO import StringIO
 from org.python.core import Py
 from com.vividsolutions.jts.geom import Polygon as JPolygon
 from org.geotools.jdbc import VirtualTable
 from org.geotools.geometry.jts import ReferencedEnvelope
 from org.geotools.referencing import CRS
 from org.geotools.factory import CommonFactoryFinder
-from org.geotools.filter import FunctionExpressionImpl
-from org.geotools.styling import SLDParser
 from org.geotools.filter.text.cql2 import CQL
 
 
 class GeoToolsMapRenderer(object):
 
 	def __init__(self):
-		pass
+		self.style_factory = CommonFactoryFinder.getStyleFactory(None)
+		self.filter_factory = CommonFactoryFinder.getFilterFactory(None)
 
-	def renderMap(map_context=None, connection_parameters=None):
+	def renderMap(self, dao=None, id_entity=None, geom_entity=None, data_entity=None, filters=[], map_context=None):
 
-		# File test.
-		"""
-		map_file = "%s/tests/test_gis_data/ne_110m_admin_1_states_provinces_shp.shp" % os.path.dirname(__file__)
-		data_file = JFile(map_file)
-
-		data_store = FileDataStoreFinder.getDataStore(data_file)
-		feature_source = data_store.getFeatureSource()
-		"""
-
-		# DB test.
-		from georefine.app import db
-		from georefine.util.sa.sa_dao import SA_DAO
-		from georefine.app.projects.util import manage_projects
-		from georefine.app.projects.models import Project
-		project_dir = '/home/adorsk/projects/gr/georefine/georefine/app/projects/util/tests/test_manage_projects_project_dir'
-		project = Project(id=1, name='test', dir=project_dir)
-		project.schema = manage_projects.getProjectSchema(project)
-		manage_projects.setUpSchema(project)		
-		#manage_projects.setUpData(project)
-
-		dao = SA_DAO(session=db.session, primary_class=project.schema.primary_class)
+		# Get connection parameters.
 		connection_parameters = dao.get_connection_parameters()
-
 		gt_to_sa_map = {
+				"dbtype": "drivername",
 				"host": "host",
 				"port": "port",
 				"database": "database",
@@ -70,53 +45,43 @@ class GeoToolsMapRenderer(object):
 		for gt_param, sa_param in gt_to_sa_map.items():
 			value = connection_parameters.get(sa_param)
 			if value:
+
+				# Handle postgres.
+				if sa_param == "drivername" and "postgresql" in value:
+					value = "postgis"
+					gt_params.put("schema", "public")
+
 				gt_params.put(gt_param, value)
-		gt_params.put("dbtype", "postgis")
-		gt_params.put("schema", "public")
 
 		data_store = DataStoreFinder.getDataStore(gt_params)
 
-		sql = "select id as geom_id, 100.0 * (id - 1.0)/(5.0 - 1.0) as value, geom from testpoly"
+		# Create raw sql query.
+		if id_entity == None:
+			id_entity = {'expression': "{%s.id}" % dao.primary_class.__name__, "label": "id"}
+		if geom_entity == None:
+			geom_entity = {'expression': "{%s.geom}.RAW" % dao.primary_class.__name__, "label": "geom"}
+		#func.ST_SetSRID(q_primary_alias.geom.RAW, 4326)
+		data_entity['label'] = 'value'
+
+		data_entities = [id_entity, geom_entity, data_entity]
+		grouping_entities = [id_entity, geom_entity]
+		sql = dao.get_sql(data_entities=data_entities, grouping_entities=grouping_entities, filters=filters)
+
+		#return
+
+		# Create VirtualTable from query.
+		#sql = "select id as geom_id, 100.0 * (id - 1.0)/(5.0 - 1.0) as value, geom from testpoly"
 		vtable = VirtualTable("vtable", sql)
-		vtable.setPrimaryKeyColumns(["geom_id"])
+		vtable.setPrimaryKeyColumns(["id"])
 		# metadatata = intententional typo. GT needs to fix the name.
 		vtable.addGeometryMetadatata("geom", JPolygon, 4326)
 
+		# Create feature source from virtual table.
 		data_store.addVirtualTable(vtable)
 		feature_source = data_store.getFeatureSource("vtable")
-		#
-
-		style = None
-
-		# Styling
-		style_factory = CommonFactoryFinder.getStyleFactory(None)
-		filter_factory = CommonFactoryFinder.getFilterFactory(None)
-
-		def create_rule(local_min, local_max, global_min, global_range, attr="value"):
-			rule = style_factory.createRule()
-			cql_filters = []
-			if not local_min == None:
-				cql_filters.append("%s >= %s" % (attr, local_min))
-			if not local_max == None:
-				cql_filters.append("%s < %s" % (attr, local_max))
-			cql = " and ".join(cql_filters)
-			gt_filter = CQL.toFilter(cql)
-			rule.setFilter(gt_filter)
-			if local_min == None:
-				brightness = 0
-			else:
-				brightness = (local_min - global_min)/global_range
-			color = Color.HSBtoRGB(0.0, 0.0, brightness)
-			fill = style_factory.createFill(
-					filter_factory.literal(color),
-					filter_factory.literal(0.5)
-					)
-			symbolizer = style_factory.createPolygonSymbolizer(None, fill, None)
-			rule.symbolizers().add(symbolizer)
-			return rule
-
 
 		# Generate class bounds.
+		# @TODO: GET FROM DATA ENTITY!, OR FROM CALCULATING!
 		num_classes = 10.0
 		vmin = 0.0
 		vmax = 100.0
@@ -127,61 +92,65 @@ class GeoToolsMapRenderer(object):
 			classes.append((vmin + i * class_width, vmin + (i + 1) * class_width))
 		classes.append((vmax, None))
 
-		# Generate rules for classes.
+		# Generate style rules for classes.
 		rules = []
 		for c in classes:
-			rule = create_rule(c[0], c[1], vmin, vrange)
+			rule = self.create_rule(c[0], c[1], vmin, vrange)
 			rules.append(rule)
+		feature_type_style = self.style_factory.createFeatureTypeStyle(rules)
+		style = self.style_factory.createStyle()
+		style.featureTypeStyles().add(feature_type_style)
 
-		fts = style_factory.createFeatureTypeStyle(rules)
-		style = style_factory.createStyle()
-		style.featureTypeStyles().add(fts)
-
-		# End Styling.
-
+		# Setup map.
+		# @TODO: FROM WMS PARMS.
 		gt_map = DefaultMapContext()
-		gt_map.setTitle('foo')
 		gt_map.addLayer(feature_source, style)
-
-
-		"""
-		print "gt map is: ", gt_map
-		JMapFrame.showMap(gt_map)
-		raw_input("foo")
-		"""
-
 		gt_renderer = StreamingRenderer()
 		gt_renderer.setMapContent(gt_map)
-
 		image_bounds = Rectangle(0, 0, 400, 400)
-
 		buffered_image = BufferedImage(image_bounds.width, image_bounds.height, BufferedImage.TYPE_INT_RGB)
 		graphics = buffered_image.createGraphics()
 		graphics.setPaint(Color.WHITE)
 		graphics.fill(image_bounds)
-
 		crs = CRS.decode("EPSG:4326")
 		map_bounds = ReferencedEnvelope(-10.0, 10.0, -10.0, 10.0, crs)
 
-
 		#gt_renderer.paint(graphics, image_bounds, gt_map.getMaxBounds())
 		gt_renderer.paint(graphics, image_bounds, map_bounds)
+
+		# Release the JDBC connection.
+		data_store.dispose()
+
+		# Return raw image.
 		byte_array_output_stream = ByteArrayOutputStream()
 		ImageIO.write(buffered_image, "jpg", byte_array_output_stream)
 		byte_array = byte_array_output_stream.toByteArray()
-
 		raw_image = Py.newString(StringUtil.fromBytes(byte_array))
-		print raw_image
+		return raw_image
 
-		"""
-		gt_map = DefaultMapContext()
+	def create_rule(self, local_min, local_max, global_min, global_range, attr="value"):
+		rule = self.style_factory.createRule()
+		cql_filters = []
+		if not local_min == None:
+			cql_filters.append("%s >= %s" % (attr, local_min))
+		if not local_max == None:
+			cql_filters.append("%s < %s" % (attr, local_max))
+		cql = " and ".join(cql_filters)
+		gt_filter = CQL.toFilter(cql)
+		rule.setFilter(gt_filter)
+		if local_min == None:
+			brightness = 0
+		else:
+			brightness = (local_min - global_min)/global_range
+		color = Color.HSBtoRGB(0.0, 0.0, brightness)
+		fill = self.style_factory.createFill(
+				self.filter_factory.literal(color),
+				self.filter_factory.literal(0.5)
+				)
+		symbolizer = self.style_factory.createPolygonSymbolizer(None, fill, None)
+		rule.symbolizers().add(symbolizer)
+		return rule
 
-		for layer in map_context.get('layers', []):
-			data_store = layer.get('data_store')
-			style = layer.get('style')
-			gt_map.addLayer(feature_source, style)
-
-		"""
 
 		
 
--- georefine/util/mapping/tests/test_gt_renderer.py
+++ georefine/util/mapping/tests/test_gt_renderer.py
@@ -1,12 +1,121 @@
 import unittest
+from georefine.util.sa.tests.basetest import BaseTest
 from georefine.util.mapping.gt_renderer import GeoToolsMapRenderer
+from georefine.util.sa.sa_dao import SA_DAO
 
-class GeoToolsMapRendererTest(unittest.TestCase):
+from sqlalchemy import Table, Column, ForeignKey, ForeignKeyConstraint, Integer, String, Float
+from sqlalchemy.orm import relationship, mapper
+from sqlalchemy import MetaData
+from geoalchemy import *
+from geoalchemy.postgis import PGComparator
+from pprint import pprint
+
+class GeoToolsMapRendererTest(BaseTest):
 
 	def testRendering(self):
+		self.dao = SA_DAO(session=self.session, primary_class=self.schema['primary_class'])
 		renderer = GeoToolsMapRenderer()
 
-		renderer.renderMap()
+		data_entity = {"expression": "func.sum({TestClass1.id})"}
+		print renderer.renderMap(dao=self.dao, data_entity=data_entity)
+	
+	def setUp(self):
+		super(GeoToolsMapRendererTest, self).setUp()
+
+		schema = {}
+		self.schema = schema
+
+		schema['classes'] = {}
+
+		class TestClass1(object):
+			id = None
+			geom = None
+			children = []
+		schema['classes']['TestClass1'] = TestClass1
+
+		class TestClass2(object):
+			id = None
+			name = ""
+		schema['classes']['TestClass2'] = TestClass2
+
+		schema['primary_class'] = TestClass1
+
+		self.metadata = MetaData()
+
+		test1_table = Table('test1', self.metadata,
+				Column('id', Integer, primary_key=True),
+				GeometryExtensionColumn('geom', Polygon(2)),
+				)
+		GeometryDDL(test1_table)
+
+		test2_table = Table('test2', self.metadata,
+				Column('id', Integer, primary_key=True),
+				Column('name', String)
+				)
+
+		test1_test2_table = Table('test1_test2', self.metadata,
+				Column('test1_id', Integer, primary_key=True),
+				Column('test2_id', Integer, primary_key=True),
+				ForeignKeyConstraint(['test1_id'], [test1_table.c.id]),
+				ForeignKeyConstraint(['test2_id'], [test2_table.c.id])
+				)
+
+		mapper(
+				TestClass1, 
+				test1_table,
+				properties = {
+					'children': relationship(TestClass2, secondary=test1_test2_table),
+					'geom': GeometryColumn(test1_table.c.geom)
+					}
+				)
+
+		mapper(
+				TestClass2, 
+				test2_table,
+				properties = {
+					}
+				)
+
+		self.metadata.drop_all(self.session.bind, checkfirst=True)
+		self.metadata.create_all(self.session.bind, checkfirst=True)
+
+		tc1s = []
+		tc2s = []
+		for i in range(5):
+
+			vertices = []
+			vertices.append([i, i])
+			vertices.append([i, i + 1])
+			vertices.append([i + 1, i + 1])
+			vertices.append([i + 1, i])
+			vertices.append(vertices[0])
+			geom = "POLYGON((%s))" % ', '.join(["%s %s" % (v[0], v[1]) for v in vertices])
+
+			tc1 = TestClass1()
+			tc1.geom = geom
+			tc1s.append(tc1)
+			self.session.add(tc1)
+
+			tc2 = TestClass2()
+			tc2.name = "tc2_%s" % i
+			tc2s.append(tc2)
+			self.session.add(tc2)
+
+		self.session.commit()
+		self.trans.commit()
+
+		"""
+		for i in range(len(tc1s)):
+			tc1 = tc1s[i]
+			child_tc2s = [tc2s[i], tc2s[ (i + 1) % len(tc1s)]]
+			for c in child_tc2s:
+				tc2 = self.session.query(TestClass2).filter(TestClass2.id == c.id).one()
+				tc1.children.append(tc2)
+		self.session.commit()
+		"""
+	def tearDown(self):
+		self.metadata.drop_all(self.session.bind, checkfirst=True)
+
 
 if __name__ == '__main__':
 	unittest.main()
