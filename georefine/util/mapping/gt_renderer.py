b8a50d3cff44d2f8f775537d4b6d14ed4856907b
adorsk <adorsk>
2012-06-12 13:30:15 -0400

    added styling

--- georefine/util/mapping/gt_renderer.py
+++ georefine/util/mapping/gt_renderer.py
@@ -18,10 +18,15 @@ from javax.imageio import ImageIO
 from org.python.core.util import StringUtil
 from StringIO import StringIO
 from org.python.core import Py
-from com.vividsolutions.jts.geom import Point as JPoint
+from com.vividsolutions.jts.geom import Polygon as JPolygon
 from org.geotools.jdbc import VirtualTable
 from org.geotools.geometry.jts import ReferencedEnvelope
 from org.geotools.referencing import CRS
+from org.geotools.factory import CommonFactoryFinder
+from org.geotools.filter import FunctionExpressionImpl
+from org.geotools.styling import SLDParser
+from org.geotools.filter.text.cql2 import CQL
+
 
 class GeoToolsMapRenderer(object):
 
@@ -71,20 +76,73 @@ class GeoToolsMapRenderer(object):
 
 		data_store = DataStoreFinder.getDataStore(gt_params)
 
-		sql = "select id as geom_id, geom from projects1_test1"
+		sql = "select id as geom_id, 100.0 * (id - 1.0)/(5.0 - 1.0) as value, geom from testpoly"
 		vtable = VirtualTable("vtable", sql)
 		vtable.setPrimaryKeyColumns(["geom_id"])
 		# metadatata = intententional typo. GT needs to fix the name.
-		vtable.addGeometryMetadatata("geom", JPoint, 4326)
+		vtable.addGeometryMetadatata("geom", JPolygon, 4326)
 
 		data_store.addVirtualTable(vtable)
 		feature_source = data_store.getFeatureSource("vtable")
 		#
 
+		style = None
+
+		# Styling
+		style_factory = CommonFactoryFinder.getStyleFactory(None)
+		filter_factory = CommonFactoryFinder.getFilterFactory(None)
+
+		def create_rule(local_min, local_max, global_min, global_range, attr="value"):
+			rule = style_factory.createRule()
+			cql_filters = []
+			if not local_min == None:
+				cql_filters.append("%s >= %s" % (attr, local_min))
+			if not local_max == None:
+				cql_filters.append("%s < %s" % (attr, local_max))
+			cql = " and ".join(cql_filters)
+			gt_filter = CQL.toFilter(cql)
+			rule.setFilter(gt_filter)
+			if local_min == None:
+				brightness = 0
+			else:
+				brightness = (local_min - global_min)/global_range
+			color = Color.HSBtoRGB(0.0, 0.0, brightness)
+			fill = style_factory.createFill(
+					filter_factory.literal(color),
+					filter_factory.literal(0.5)
+					)
+			symbolizer = style_factory.createPolygonSymbolizer(None, fill, None)
+			rule.symbolizers().add(symbolizer)
+			return rule
+
+
+		# Generate class bounds.
+		num_classes = 10.0
+		vmin = 0.0
+		vmax = 100.0
+		vrange = vmax - vmin
+		class_width = vrange/num_classes
+		classes = [(None, vmin)]
+		for i in range(num_classes):
+			classes.append((vmin + i * class_width, vmin + (i + 1) * class_width))
+		classes.append((vmax, None))
+
+		# Generate rules for classes.
+		rules = []
+		for c in classes:
+			rule = create_rule(c[0], c[1], vmin, vrange)
+			rules.append(rule)
+
+		fts = style_factory.createFeatureTypeStyle(rules)
+		style = style_factory.createStyle()
+		style.featureTypeStyles().add(fts)
+
+		# End Styling.
 
 		gt_map = DefaultMapContext()
 		gt_map.setTitle('foo')
-		gt_map.addLayer(feature_source, None)
+		gt_map.addLayer(feature_source, style)
+
 
 		"""
 		print "gt map is: ", gt_map
@@ -104,6 +162,7 @@ class GeoToolsMapRenderer(object):
 
 		crs = CRS.decode("EPSG:4326")
 		map_bounds = ReferencedEnvelope(-10.0, 10.0, -10.0, 10.0, crs)
+
 
 		#gt_renderer.paint(graphics, image_bounds, gt_map.getMaxBounds())
 		gt_renderer.paint(graphics, image_bounds, map_bounds)
