7cfba6a1f4cf4988e304a0f7405ebc88a97cd6e7
adorsk <adorsk>
2012-07-17 17:10:20 -0400

    added fetching timeout for summary bar

--- georefine/app/static/js/GeoRefineClient/src/views/GeoRefineClient.js
+++ georefine/app/static/js/GeoRefineClient/src/views/GeoRefineClient.js
@@ -1103,12 +1103,32 @@ function($, Backbone, _, ui, _s, Facets, MapView, Charts, Windows, Util, templat
 
                     // Get data when parameters change.
                     if (this.model.getData){
+
+                        // helper function to get a timeout getData function.
+                        var _timeoutGetData = function(){
+                            var delay = 500;
+                            return setTimeout(function(){
+                                console.log("getData", model.id, arguments);
+                                model.getData();
+                                model.set('_fetch_timeout', null);
+                            }, delay);
+                        };
+
                         this.model.on('change:primary_filters change:base_filters change:quantity_field', function(){
-                            console.log("getData", model.id, arguments);
-                            this.model.getData();
-                        }, this);
+
+                            // We delay the get data call a little, in case multiple things are changing.
+                            // The last change will get executed.
+                            var fetch_timeout = model.get('_fetch_timeout');
+                            // If we're fetching, clear the previous fetch.
+                            if (fetch_timeout){
+                                clearTimeout(fetch_timeout);
+                            }
+                            // Start a new fetch.
+                            model.set('_fetch_timeout', _timeoutGetData(arguments));
+                        });
                     }
 				},
+
 				onDataChange: function(){
 					var format = this.model.get('quantity_field').get('format') || "%s";
 					var data = this.model.get('data');
