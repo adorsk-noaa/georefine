16948ad9177287442c5e6d35b5961011f988cc67
adorsk <adorsk>
2012-07-18 08:57:28 -0400

    added creation of geom entities for testing using just tables

--- georefine/util/mapping/tests/test_gt_renderer.py
+++ georefine/util/mapping/tests/test_gt_renderer.py
@@ -12,117 +12,96 @@ from pprint import pprint
 
 class GeoToolsMapRendererTest(BaseTest):
 
-	def testRendering(self):
-		self.dao = SA_DAO(session=self.session, primary_class=self.schema['primary_class'])
-		renderer = GeoToolsMapRenderer()
+    def testRendering(self):
+        schema = self.setUpSchemaAndData()
+        self.dao = SA_DAO(connection=self.connection, schema=schema)
+        renderer = GeoToolsMapRenderer()
 
-		data_entity = {"expression": "func.sum({TestClass1.id})", "num_classes": 5, "min": 1, "max": 5}
-		map_parameters = {
-				"width": 600,
-				"height": 600,
-				"bbox": "-10,-10,10,10",
-				"format": "image/png",
-				"transparent": True
-				}
-		print renderer.renderMap(dao=self.dao, data_entity=data_entity, map_parameters=map_parameters)
-	
-	def setUp(self):
-		super(GeoToolsMapRendererTest, self).setUp()
+        """
+        #data_entity = {"expression": "func.sum({TestClass1.id})", "num_classes": 5, "min": 1, "max": 5}
+        map_parameters = {
+                "width": 600,
+                "height": 600,
+                "bbox": "-10,-10,10,10",
+                "format": "image/png",
+                "transparent": True
+                }
+        print renderer.renderMap(dao=self.dao, data_entity=data_entity, map_parameters=map_parameters)
+        """
+    
+    def setUpSchemaAndData(self):
 
-		schema = {}
-		self.schema = schema
+        schema = {}
+        tables = {}
+        ordered_tables = []
 
-		schema['classes'] = {}
+        metadata = MetaData()
 
-		class TestClass1(object):
-			id = None
-			geom = None
-			children = []
-		schema['classes']['TestClass1'] = TestClass1
+        tables['test1'] = Table('test1', metadata,
+                Column('id', Integer, primary_key=True),
+                Column('name', String),
+                GeometryExtensionColumn('geom', Polygon(2)),
+                )
+        GeometryDDL(tables['test1'])
+        ordered_tables.append(tables['test1'])
 
-		class TestClass2(object):
-			id = None
-			name = ""
-		schema['classes']['TestClass2'] = TestClass2
+        tables['test2'] = Table('test2', metadata,
+                Column('id', Integer, primary_key=True),
+                Column('name', String)
+                )
+        ordered_tables.append(tables['test2'])
 
-		schema['primary_class'] = TestClass1
+        tables['test1_test2'] = Table('test1_test2', metadata,
+                Column('test1_id', Integer, primary_key=True),
+                Column('test2_id', Integer, primary_key=True),
+                ForeignKeyConstraint(['test1_id'], ['test1.id']),
+                ForeignKeyConstraint(['test2_id'], ['test2.id'])
+                )
+        ordered_tables.append(tables['test1_test2'])
 
-		self.metadata = MetaData()
+        schema['tables'] = tables
+        schema['ordered_tables'] = tables
 
-		test1_table = Table('test1', self.metadata,
-				Column('id', Integer, primary_key=True),
-				GeometryExtensionColumn('geom', Polygon(2)),
-				)
-		GeometryDDL(test1_table)
+        metadata.create_all(self.connection)
 
-		test2_table = Table('test2', self.metadata,
-				Column('id', Integer, primary_key=True),
-				Column('name', String)
-				)
+        t1s = []
+        t2s = []
+        for i in range(5):
 
-		test1_test2_table = Table('test1_test2', self.metadata,
-				Column('test1_id', Integer, primary_key=True),
-				Column('test2_id', Integer, primary_key=True),
-				ForeignKeyConstraint(['test1_id'], [test1_table.c.id]),
-				ForeignKeyConstraint(['test2_id'], [test2_table.c.id])
-				)
+            vertices = []
+            vertices.append([i, i])
+            vertices.append([i, i + 1])
+            vertices.append([i + 1, i + 1])
+            vertices.append([i + 1, i])
+            vertices.append(vertices[0])
+            wkt_geom = "POLYGON((%s))" % ', '.join(["%s %s" % (v[0], v[1]) for v in vertices])
 
-		mapper(
-				TestClass1, 
-				test1_table,
-				properties = {
-					'children': relationship(TestClass2, secondary=test1_test2_table),
-					'geom': GeometryColumn(test1_table.c.geom)
-					}
-				)
+            t1 = {
+                "id": i,
+                "name": "t1_%s" % i,
+                "geom": WKTSpatialElement(wkt_geom)
+            }
+            t1s.append(t1)
+            self.connection.execute(schema['tables']['test1'].insert().values(**t1))
 
-		mapper(
-				TestClass2, 
-				test2_table,
-				properties = {
-					}
-				)
+            t2 = {
+                "id": i,
+                "name": "t2_%s" % i,
+            }
+            t2s.append(t2)
+            self.connection.execute(schema['tables']['test2'].insert().values(**t2))
 
-		self.metadata.drop_all(self.session.bind, checkfirst=True)
-		self.metadata.create_all(self.session.bind, checkfirst=True)
+        for i in range(len(t1s)):
+            t1 = t1s[i]
+            child_t2s = [t2s[i], t2s[ (i + 1) % len(t1s)]]
+            for t2 in child_t2s:
+                t1_t2 = {
+                        "test1_id": t1['id'],
+                        "test2_id": t2['id']
+                        }
+                self.connection.execute(schema['tables']['test1_test2'].insert().values(**t1_t2))
 
-		tc1s = []
-		tc2s = []
-		for i in range(5):
-
-			vertices = []
-			vertices.append([i, i])
-			vertices.append([i, i + 1])
-			vertices.append([i + 1, i + 1])
-			vertices.append([i + 1, i])
-			vertices.append(vertices[0])
-			geom = "POLYGON((%s))" % ', '.join(["%s %s" % (v[0], v[1]) for v in vertices])
-
-			tc1 = TestClass1()
-			tc1.geom = geom
-			tc1s.append(tc1)
-			self.session.add(tc1)
-
-			tc2 = TestClass2()
-			tc2.name = "tc2_%s" % i
-			tc2s.append(tc2)
-			self.session.add(tc2)
-
-		self.session.commit()
-		self.trans.commit()
-
-		"""
-		for i in range(len(tc1s)):
-			tc1 = tc1s[i]
-			child_tc2s = [tc2s[i], tc2s[ (i + 1) % len(tc1s)]]
-			for c in child_tc2s:
-				tc2 = self.session.query(TestClass2).filter(TestClass2.id == c.id).one()
-				tc1.children.append(tc2)
-		self.session.commit()
-		"""
-	def tearDown(self):
-		self.metadata.drop_all(self.session.bind, checkfirst=True)
-
+        return schema
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
