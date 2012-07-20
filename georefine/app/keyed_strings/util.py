39d3b777d8917eff1b097604c57db9987565c7fa
adorsk <adorsk>
2012-07-20 10:43:46 -0400

    took out print statement

--- georefine/app/keyed_strings/util.py
+++ georefine/app/keyed_strings/util.py
@@ -6,7 +6,6 @@ def getKey(s):
     key = hashlib.md5(s).hexdigest()
     ks = db.session.query(KeyedString).filter(KeyedString.key == key).first()
     if not ks:
-        print "saving"
         ks = KeyedString(key = key, s = s)
         db.session.add(ks)
         db.session.commit()
