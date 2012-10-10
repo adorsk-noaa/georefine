34781270b2b766439027e1e0d2cf0f49952bcb6e
adorsk <adorsk>
2012-10-10 13:34:54 -0400

    fixed static urls

--- georefine/app/static/js/require_config.js
+++ georefine/app/static/js/require_config.js
@@ -1,26 +1,32 @@
-var CONTEXT_BASE = '/georefine';
-var ASSETS_BASEURL = CONTEXT_BASE + '/static/sasi_assets';
-var JQPLOT_BASE = '/js/jqplot';
+if (typeof STATIC_BASEURL == 'undefined'){
+    STATIC_BASEURL = '/static';
+}
+
+if (typeof ASSETS_BASEURL == 'undefined'){
+    ASSETS_BASEURL = STATIC_BASEURL + 'sasi_assets/';
+}
+
+var JQPLOT_BASE = 'js/jqplot';
 
 var require = {
 	deps: [],
 
 	paths: {
-		text: ASSETS_BASEURL + "/js/require.js/plugins/text",
-		use: ASSETS_BASEURL + "/js/require.js/plugins/use",
-		jquery: ASSETS_BASEURL + "/js/jquery",
-		underscore: ASSETS_BASEURL + "/js/underscore",
-		backbone: ASSETS_BASEURL + "/js/backbone",
-		ui: ASSETS_BASEURL + "/js/jquery.ui/jquery-ui",
-		_s: ASSETS_BASEURL + "/js/underscore.string",
-		openlayers: ASSETS_BASEURL + "/js/openlayers/openlayers",
+		text: ASSETS_BASEURL + "js/require.js/plugins/text",
+		use: ASSETS_BASEURL + "js/require.js/plugins/use",
+		jquery: ASSETS_BASEURL + "js/jquery",
+		underscore: ASSETS_BASEURL + "js/underscore",
+		backbone: ASSETS_BASEURL + "js/backbone",
+		ui: ASSETS_BASEURL + "js/jquery.ui/jquery-ui",
+		_s: ASSETS_BASEURL + "js/underscore.string",
+		openlayers: ASSETS_BASEURL + "js/openlayers/openlayers",
 		jqplot: ASSETS_BASEURL + JQPLOT_BASE + "/jquery.jqplot.min",
 		jqp_bar: ASSETS_BASEURL + JQPLOT_BASE + "/plugins/jqplot.barRenderer.min",
 		jqp_cat_axis_renderer: ASSETS_BASEURL + JQPLOT_BASE + "/plugins/jqplot.categoryAxisRenderer.min",
 		jqp_log_axis_renderer: ASSETS_BASEURL + JQPLOT_BASE + "/plugins/jqplot.logAxisRenderer.min",
-		flot: ASSETS_BASEURL + "/js/flot/jquery.flot",
-		jqwindow: ASSETS_BASEURL + "/js/jquery.window/jquery.window",
-		qtip: ASSETS_BASEURL + "/js/jquery.qtip/jquery.qtip"
+		flot: ASSETS_BASEURL + "js/flot/jquery.flot",
+		jqwindow: ASSETS_BASEURL + "js/jquery.window/jquery.window",
+		qtip: ASSETS_BASEURL + "js/jquery.qtip/jquery.qtip"
 	},
 	
 	use: {
@@ -82,57 +88,57 @@ var require = {
 	packages: [
 		{
 		  "name": "FacetApp",
-		  "location": ASSETS_BASEURL + "/js/facet_app/src"
+		  "location": ASSETS_BASEURL + "js/facet_app/src"
 		},
 
 		{
 		  "name": "Facets",
-		  "location": ASSETS_BASEURL + "/js/facets/src"
+		  "location": ASSETS_BASEURL + "js/facets/src"
 		},
 
 		{
 		  "name": "MapView",
-		  "location": ASSETS_BASEURL + "/js/mapview/src"
+		  "location": ASSETS_BASEURL + "js/mapview/src"
 		},
 		
 		{
 		  "name": "Dialogs",
-		  "location": ASSETS_BASEURL + "/js/dialogs/src"
+		  "location": ASSETS_BASEURL + "js/dialogs/src"
 		},
 
 		{
 		  "name": "ExportDialog",
-		  "location": ASSETS_BASEURL + "/js/export_dialog/src"
+		  "location": ASSETS_BASEURL + "js/export_dialog/src"
 		},
 
 		{
 		  "name": "Charts",
-		  "location": ASSETS_BASEURL + "/js/charts/src"
+		  "location": ASSETS_BASEURL + "js/charts/src"
 		},
 
 		{
 		  "name": "Util",
-		  "location": ASSETS_BASEURL + "/js/util/src"
+		  "location": ASSETS_BASEURL + "js/util/src"
 		},
 
 		{
 		  "name": "Windows",
-		  "location": ASSETS_BASEURL + "/js/windows/src"
+		  "location": ASSETS_BASEURL + "js/windows/src"
 		},
 
 		{
 		  "name": "GeoRefineClient",
-		  "location": CONTEXT_BASE + "/static/js/GeoRefineClient/src"
+		  "location": STATIC_BASEURL + "js/GeoRefineClient/src"
 		},
 
         {
             "name": "uiExtras",
-            "location": ASSETS_BASEURL + "/js/jquery.ui.extras"
+            "location": ASSETS_BASEURL + "js/jquery.ui.extras"
         },
 
         {
             "name": "Menus",
-            "location": ASSETS_BASEURL + "/js/menus/src"
+            "location": ASSETS_BASEURL + "js/menus/src"
         }
 	]
 
