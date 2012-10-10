811f131f6e1fe0aea71c95326ca1b3624fc4cf56
adorsk <adorsk>
2012-10-10 13:35:10 -0400

    read static dir, static url path from config

--- georefine/app/__init__.py
+++ georefine/app/__init__.py
@@ -4,15 +4,36 @@ from flask import Flask, render_template, escape
 from flask_admin import Admin
 
 import os
+import imp
 
 
-# Setup config stuff first to avoid circular import issues.
+# Import instance config if it exists before app is created, in order to get
+# static dirs.
 this_dir = os.path.dirname(os.path.abspath(__file__))
+instance_config_filename = os.path.join(this_dir, 'instance', 'app_config.py')
+instance_config = imp.new_module('instance_config')
+instance_config.__file__ = instance_config_filename
+try:
+    execfile(instance_config_filename, instance_config.__dict__)
+except IOError, e:
+    pass
 
-app = Flask(__name__, instance_path=os.path.join(this_dir, 'instance'))
+app_kwargs = {}
+app_attrs = ['static_folder', 'static_url_path']
+for attr in app_attrs:
+    uc_attr = attr.upper()
+    value = getattr(instance_config, uc_attr, None) \
+            or getattr(config, uc_attr, None)
+    if value is not None:
+        app_kwargs[attr] = value
+
+# Initialize app.
+app = Flask(__name__, instance_path=os.path.join(this_dir, 'instance'), 
+            **app_kwargs)
+
+# Setup app config.
 app.config.from_object(config)
-app.config.from_pyfile(os.path.join(app.instance_path, 'app_config.py'),
-                       silent=True)
+app.config.from_object(instance_config)
 
 import db
 
