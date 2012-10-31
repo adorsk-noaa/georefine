2e8dad1b513f57efe2d267e368667f35cb7a965c
adorsk <adorsk>
2012-10-31 15:04:18 -0400

    added assetfile stub

---
+++ georefine/app/static/js/GeoRefineClient/AssetFile.py
@@ -0,0 +1,108 @@
+config = {
+    'CACHE_DIR': 'cache',
+    'TARGET_DIR': 'assets'
+}
+
+assets = {
+    'backbone.js': {
+        'type': 'git',
+        'source': 'https://github.com/documentcloud/backbone.git',
+        'path': 'backbone.js'
+    },
+    'charts': {
+        'type': 'git',
+        'source': 'https://github.com/adorsk-noaa/bb_charts.git'
+    },
+    'commonStyles': {
+        'type': 'git',
+        'source': 'https://github.com/adorsk-noaa/commonStyles.git'
+    },
+    'DataTables': {
+        'type': 'git',
+        'source': 'https://github.com/DataTables/DataTables.git'
+    },
+    'dialogs': {
+        'type': 'git',
+        'source': 'https://github.com/adorsk-noaa/bb_dialogs.git'
+    },
+    'export_dialog': {
+        'type': 'git',
+        'source': 'https://github.com/adorsk-noaa/export_dialog.git'
+    },
+    'facets': {
+        'type': 'git',
+        'source': 'https://github.com/adorsk-noaa/bb_facets.git'
+    },
+    'jqplot': {
+        'type': 'hg',
+        'source': 'ssh://hg@bitbucket.org/cleonello/jqplot'
+    },
+    'jquery.form.js': {
+        'type': 'git',
+        'source': 'https://github.com/malsup/form.git',
+        'path': 'jquery.form.js'
+    },
+    'jquery.js': {
+        'type': 'url',
+        'source': 'https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js'
+    },
+    'jquery.qtip': {
+        'type': 'git',
+        'source': 'https://github.com/Craga89/qTip2.git',
+        'path': 'dist/jquery.qtip.js'
+    },
+    'jquery.ui': {
+        'type': 'url',
+        'source': 'https://github.com/downloads/jquery/jquery-ui/jquery-ui-1.9.1.zip'
+    },
+    'jquery.ui.extras': {
+        'type': 'git',
+        'source': 'https://github.com/adorsk-noaa/jquery.ui.extras.git'
+    },
+    'jquery.window': {
+        'type': 'git',
+        'source': 'https://github.com/adorsk/jquery.window.git'
+    },
+    'less.js': {
+        'type': 'git',
+        'source': 'https://github.com/adorsk/less.js.git'
+    },
+    'mapview': {
+        'type': 'git',
+        'source': 'https://github.com/adorsk-noaa/bb_mapview.git'
+    },
+    'menus': {
+        'type': 'git',
+        'source': 'https://github.com/adorsk-noaa/bb_menus.git'
+    },
+    'openlayers': {
+        'type': 'git',
+        'source': 'https://github.com/openlayers/openlayers.git'
+    },
+    'require.js': {
+        'type': 'git',
+        'source': 'https://github.com/jrburke/requirejs.git',
+        'path': 'require.js'
+    },
+    'rless.js': {
+        'type': 'git',
+        'source': 'https://github.com/adorsk/requirejs-less.git',
+        'path': 'requirejs-less.js'
+    },
+    'underscore.js': {
+        'type': 'git',
+        'source': 'https://github.com/documentcloud/underscore.git'
+    }
+    'underscore.string.js': {
+        'type': 'git',
+        'source': 'https://github.com/epeli/underscore.string.git'
+    },
+    'util': {
+        'type': 'git',
+        'source': 'https://github.com/adorsk-noaa/georefine_util.git'
+    },
+    'windows': {
+        'type': 'git',
+        'source': 'https://github.com/adorsk-noaa/bb_window.git'
+    }
+}
