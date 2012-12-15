5617d4163fef389ecdced7df84f23932ad84abc3
adorsk <adorsk>
2012-12-15 10:35:17 -0500

    fixing up create project

--- georefine/app/projects/util/services.py
+++ georefine/app/projects/util/services.py
@@ -246,8 +246,8 @@ def create_project(input_path=None, logger=logging.getLogger(),
         tmp_dir = None
         if input_path.endswith('.tar.gz') or input_path.endswith('.tgz'):
             tmp_dir = tempfile.mkdtemp(prefix="gr.prj_%s." % project.id)
-            tar = tarfile.open(project_file)
-            tar.extractall(src_dir)
+            tar = tarfile.open(input_path)
+            tar.extractall(tmp_dir)
             tar.close()
             src_dir = tmp_dir
         else:
